19866cb0e2375e68bf603c837757cdfc8598b2fc
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-09-06 23:22:22 +0530

    Folder structure phase 1 done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import './App.css';
+import { Routes, Route} from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="App">
+
+      <div className="pages">
+        <Routes>
+          {/* <Route to='/' element={} /> */}
+        </Routes>
+      </div>
       APP
     </div>
   )
