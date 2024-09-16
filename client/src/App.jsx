44388935b376430cccfcb2c5b24f751e138c1295
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-09-16 22:06:28 +0530

    Header component added to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
-import './App.scss';
-import { Routes, Route} from 'react-router-dom';
-import { HomePage } from './pages/homepage/homepage.component';
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/homepage/homepage.component";
+import { Header } from "./components/header/header.component";
 
 const App = () => {
   return (
     <div className="App">
-
+      <Header />
       <div className="pages">
         <Routes>
-          <Route path='/' element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
---
+++ client/src/components/header/header.component.jsx
@@ -0,0 +1,9 @@
+import "./header.styles.scss";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <h2>Header</h2>
+    </div>
+  );
+};
